1552bd96e7d1ce7e6f0da34a3478f61e77ab35aa
Junho Yeo <i@junho.io>
2022-10-11 20:35:06 +0900

    Implement `Options` & `StoreClient`

--- index.ts
+++ index.ts
@@ -1,44 +1,61 @@
 import CompressedJSON from 'compressed-json';
-import { createClient } from 'redis';
 
-type RedisClient = ReturnType<typeof createClient>;
+type StoreClient = {
+  get: (key: string) => Promise<any>;
+  set: (key: string, value: any) => Promise<void>;
+};
 
 type CacheVO<T extends any> = {
   t: number;
   v: T;
 };
 const getCached = async <T extends any>(
-  __key: string,
-  __redisClient: RedisClient,
+  key: string,
+  storeClient: StoreClient,
 ) => {
-  const cachedRawValue = await __redisClient.get(__key);
-  if (!cachedRawValue) {
-    return null;
-  }
-  return CompressedJSON.decompress.fromString<T>(cachedRawValue);
+  const cachedRawValue = await storeClient.get(key);
+  return !cachedRawValue
+    ? null
+    : CompressedJSON.decompress.fromString<T>(cachedRawValue);
 };
 
-export const withRedisCache = async <T extends any>(
+type Options<T extends any> = {
+  storeClient: StoreClient;
+
+  // cache time
+  ttl?: number;
+
+  fallbackValue?: T;
+  fallbackFunction?: (() => T) | (() => Promise<T>);
+  onError?: (err: Error) => void;
+};
+
+export const withReliableCache = async <T extends any>(
   key: string,
-  asyncOperation: () => Promise<T>,
-  redisClient: RedisClient,
-  cacheTime: number,
-  fallbackValue?: T,
-  fallbackFunction?: () => Promise<T>,
+  asyncOperation: Promise<T> | (() => Promise<T>),
+  {
+    storeClient,
+    ttl = 3_000,
+    fallbackValue,
+    fallbackFunction,
+    onError,
+  }: Options<T>,
 ) => {
   let value: T | null = null;
   let cachedAt: number | null = 0;
 
-  const cached = await getCached<CacheVO<T>>(key, redisClient);
-  if (cached && cached.t >= Date.now() - cacheTime) {
+  const cached = await getCached<CacheVO<T>>(key, storeClient);
+  if (cached && cached.t >= Date.now() - ttl) {
     // Use cached value if not expired
     value = cached.v;
     cachedAt = cached.t;
   } else {
     try {
-      value = await asyncOperation();
+      value = await (asyncOperation instanceof Promise
+        ? asyncOperation
+        : asyncOperation());
       cachedAt = new Date().getTime();
-      await redisClient.set(
+      await storeClient.set(
         key,
         CompressedJSON.compress.toString<CacheVO<T>>({
           v: value,
@@ -46,19 +63,18 @@ export const withRedisCache = async <T extends any>(
         }),
       );
     } catch (err) {
-      console.error(err);
-
-      if (!cached) {
-        if (fallbackFunction) {
+      onError?.(err);
+      if (!!cached) {
+        // Use cached value if available
+        value = cached.v;
+        cachedAt = cached.t;
+      } else {
+        if (!!fallbackFunction) {
           value = await fallbackFunction();
         } else {
           value = fallbackValue || null;
         }
         cachedAt = null;
-      } else {
-        // Use cached value if available
-        value = cached.v;
-        cachedAt = cached.t;
       }
     }
   }
